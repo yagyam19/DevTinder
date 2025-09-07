1baa782524624962f4aa46b42290abd65a0964ee
Yagyam Patidar <ypatidar@grepruby.io>
2025-09-07 20:41:54 +0530

    WIP: routing patterns

--- src/app.js
+++ src/app.js
@@ -2,18 +2,39 @@ const express = require('express');
 
 const app = express();
 
+// app.use('/profile', (req, res) => {
+//   res.send('profile is here');
+// })
+
+// ********* Advanced routing concept
+// app.get('/ab?c', (req, res) => {
+//   res.send({ firstName: 'Yagyam', lastName: 'Patidar', rank: 10 });
+// });
+
+// Matches both /abc and /bc
+// app.get('ab+c', (req, res) => {
+//   res.send("Matched abc or bc");
+// });
+
+// this will match all the get api calls to this route.
+app.get('/profile/:userId', (req, res) => {
+  console.log(req.query, req.params);
+  res.send({ firstName: 'Yagyam', lastName: 'Patidar', rank: 10 });
+});
+
+app.post('/profile', (req, res) => {
+  res.send({ message: 'Record created successfully.' });
+});
+
+app.delete('/profile', (req, res) => {
+  res.send({ message: 'Profile deleted successfully.' });
+});
+
+// this will match to all the http method API calls to /test
 app.use('/test', (req, res) => {
   // request handler
   res.send('Hello Dev Tinder');
 });
-
-app.use('/welcome', (req, res) => {
-  res.send('Welcome to node world.');
-});
-
-app.use('/posts', (req, res) => {
-      res.send('Here you can see all posts');
-})
 
 const PORT = 3000;
 app.listen(PORT, () => {
